62adfe2f8decf45ce1e476fa9fd655342c84971b
grambaba <ganeshmdes18@gmail.com>
2021-01-31 23:20:02 +0530

    Keyshot Web Test
    Testing of Keyshot Web Doc. More tests on the way

---
+++ keyshot-configurator-imagemapping.js
@@ -0,0 +1,315 @@
+imagemappingdata =
+[
+    {
+        "image": "render_1-3.1.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "24+66",
+            "3+83",
+            "4+99",
+            "5+108"
+        ],
+        "studio": 550920123
+    },
+    {
+        "image": "render_1-3.2.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "24+66",
+            "3+84",
+            "4+99",
+            "5+108"
+        ],
+        "studio": 550920123
+    },
+    {
+        "image": "render_1-3.3.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "24+66",
+            "3+91",
+            "4+99",
+            "5+108"
+        ],
+        "studio": 550920123
+    },
+    {
+        "image": "render_1-3.4.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "24+66",
+            "3+83",
+            "4+99",
+            "5+110"
+        ],
+        "studio": 550920123
+    },
+    {
+        "image": "render_1-3.5.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "24+66",
+            "3+84",
+            "4+99",
+            "5+110"
+        ],
+        "studio": 550920123
+    },
+    {
+        "image": "render_1-3.6.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "24+66",
+            "3+91",
+            "4+99",
+            "5+110"
+        ],
+        "studio": 550920123
+    },
+    {
+        "image": "render_1-3.7.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "24+66",
+            "3+83",
+            "4+99",
+            "5+121"
+        ],
+        "studio": 550920123
+    },
+    {
+        "image": "render_1-3.8.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "24+66",
+            "3+84",
+            "4+99",
+            "5+121"
+        ],
+        "studio": 550920123
+    },
+    {
+        "image": "render_1-3.9.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "24+66",
+            "3+91",
+            "4+99",
+            "5+121"
+        ],
+        "studio": 550920123
+    },
+    {
+        "image": "render_1-3.10.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "24+66",
+            "3+83",
+            "4+100",
+            "5+121"
+        ],
+        "studio": 550920123
+    },
+    {
+        "image": "render_1-3.11.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "24+66",
+            "3+84",
+            "4+100",
+            "5+121"
+        ],
+        "studio": 550920123
+    },
+    {
+        "image": "render_1-3.12.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "24+66",
+            "3+91",
+            "4+100",
+            "5+121"
+        ],
+        "studio": 550920123
+    },
+    {
+        "image": "render_1-3.13.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "24+66",
+            "3+83",
+            "4+99",
+            "5+108"
+        ],
+        "studio": 550920122
+    },
+    {
+        "image": "render_1-3.14.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "24+66",
+            "3+84",
+            "4+99",
+            "5+108"
+        ],
+        "studio": 550920122
+    },
+    {
+        "image": "render_1-3.15.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "24+66",
+            "3+91",
+            "4+99",
+            "5+108"
+        ],
+        "studio": 550920122
+    },
+    {
+        "image": "render_1-3.16.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "24+66",
+            "3+83",
+            "4+99",
+            "5+110"
+        ],
+        "studio": 550920122
+    },
+    {
+        "image": "render_1-3.17.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "24+66",
+            "3+84",
+            "4+99",
+            "5+110"
+        ],
+        "studio": 550920122
+    },
+    {
+        "image": "render_1-3.18.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "24+66",
+            "3+91",
+            "4+99",
+            "5+110"
+        ],
+        "studio": 550920122
+    },
+    {
+        "image": "render_1-3.19.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "24+66",
+            "3+83",
+            "4+99",
+            "5+121"
+        ],
+        "studio": 550920122
+    },
+    {
+        "image": "render_1-3.20.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "24+66",
+            "3+84",
+            "4+99",
+            "5+121"
+        ],
+        "studio": 550920122
+    },
+    {
+        "image": "render_1-3.21.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "24+66",
+            "3+91",
+            "4+99",
+            "5+121"
+        ],
+        "studio": 550920122
+    },
+    {
+        "image": "render_1-3.22.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "24+66",
+            "3+83",
+            "4+100",
+            "5+121"
+        ],
+        "studio": 550920122
+    },
+    {
+        "image": "render_1-3.23.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "24+66",
+            "3+84",
+            "4+100",
+            "5+121"
+        ],
+        "studio": 550920122
+    },
+    {
+        "image": "render_1-3.24.jpg",
+        "modelsets": [
+            1005
+        ],
+        "multimaterials": [
+            "24+66",
+            "3+91",
+            "4+100",
+            "5+121"
+        ],
+        "studio": 550920122
+    }
+];
